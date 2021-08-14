ed548e35cb05a070a2caa99b0157aa9cd3bf26e6
Anastasiya Esakova <nastya77esakova@yandex.ru>
2021-08-14 21:29:16 +0300

    fix: изменение ридми, корректировка адаптива и модификаторов

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-icon');
 let popup = document.querySelector('.popup')
 
-
-
 editButton.addEventListener('click', function(){
   popup.classList.remove('popup_not-active');
 });
@@ -16,14 +14,13 @@ let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__form_el_name');
 let jobInput = document.querySelector('.popup__form_el_job');
 
-
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
   let name = document.querySelector('.profile__name');
   let job = document.querySelector('.profile__description');
   name.textContent = nameInput.value;
   job.textContent = jobInput.value;
+  popup.classList.add('popup_not-active');
 }
 formElement.addEventListener('submit', formSubmitHandler);
 
