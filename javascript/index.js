260bdf9b5add04cb085a849713750c2e246c7906
Anastasiya Esakova <nastya77esakova@yandex.ru>
2021-08-16 18:17:57 +0300

    refactor: исправление ошибок в размерах и позиционировании

--- javascript/index.js
+++ javascript/index.js
@@ -4,23 +4,32 @@ let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input_el_name');
 let jobInput = formElement.querySelector('.popup__input_el_job');
-let name = document.querySelector('.profile__name');
-let job = document.querySelector('.profile__description');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
 
-function popupActiveMode(){
-  popup.classList.toggle('popup_active');
-  nameInput.textContent = nameInput.value;
-  jobInput.textContent = jobInput.value;
+
+function popupOpenMode(){
+  popup.classList.add('popup_opened');
+  nameInput.textContent = profileName.textContent;
+  jobInput.textContent = profileJob.textContent;
+  // добавление класса и перенос имени из профиля в форму
+}
+function popupCloseMode(){
+  popup.classList.remove('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  // удаление класса и обнуление формы
 }
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  name.textContent = nameInput.value;
-  job.textContent = jobInput.value;
-  popup.classList.remove('popup_active');
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
+  // изменение данных профиля
 }
 
-editButton.addEventListener('click', popupActiveMode);
-closeButton.addEventListener('click', popupActiveMode);
+editButton.addEventListener('click', popupOpenMode);
+closeButton.addEventListener('click', popupCloseMode);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
